ed40d0c7f074fd853c5dc4afef190a0ae4c4467e
xpetrus <phsiao24@gmail.com>
2019-03-16 11:19:52 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 //connection to database
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "mathbook24",
-    database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'mathbook24',
+        database: 'burgers_db'
+    });
+};
 
 connection.connect(function(err){
     if(err){
